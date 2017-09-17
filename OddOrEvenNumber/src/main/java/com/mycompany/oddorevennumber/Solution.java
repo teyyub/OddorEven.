1272970499c0ca11a1859016d68088cf55951058
apollo1986 <teyyubeyyuboglu@gmail.com>
2017-09-17 18:30:45 +0400

    first version

---
+++ OddOrEvenNumber/src/main/java/com/mycompany/oddorevennumber/Solution.java
@@ -0,0 +1,17 @@
+ 
+
+package com.mycompany.oddorevennumber;
+
+/**
+ *
+ * @author teyyub Sep 17, 2017 6:28:15 PM
+ */
+public class Solution {
+//Write an application that reads an integer and determines and prints whether it is odd or even.
+//[Hint: Use the modulus operator. An even number is a multiple of 2. Any multiple of 2 leaves a remainder
+//of 0 when divided by 2.]
+    
+    public static void main(String[] args) {
+        System.out.println("test");  
+    }
+}
